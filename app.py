5c2d9d45da062d214a1f305db8cbf1ffea2979b5
Praveenajw <praveenajw@gmail.com>
2025-07-27 22:40:03 +0530

    file added

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import joblib
 
 app = Flask(__name__)
 
-# Load the trained model and the interest mapping
+
 model = joblib.load('model.pkl')
 interest_map = joblib.load('interest_map.pkl')
 
-# Make a new dictionary to change numbers back to interest names
+
 interest_map_rev = {}
 for interest, number in interest_map.items():
     interest_map_rev[number] = interest
@@ -15,15 +15,13 @@ for interest, number in interest_map.items():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     prediction = None
-    if request.method == 'POST':
-        # Get age and gender from the form
+    if request.method == 'POST':     
         age = int(request.form['age'])
         gender = int(request.form['gender'])
-        # Use the model to predict the interest number
         pred_num = model.predict([[age, gender]])[0]
-        # Get the interest name from the number
+
         prediction = interest_map_rev.get(pred_num, "Unknown")
-    # Show the result on the web page
+
     return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
--- train_model.py
+++ train_model.py
@@ -2,27 +2,25 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 import joblib
 
-# 1. Load the data from CSV file
+
 data = pd.read_csv('Movie_Interests.csv')
 
-# 2. Give each unique interest a number
-interest_names = data['Interest'].unique()  # Get all unique interests
-interest_map = {}  # Create an empty dictionary
-for i in range(len(interest_names)):
-    interest_map[interest_names[i]] = i  # Assign a number to each interest
 
-# 3. Replace interest names with numbers in the data
+interest_names = data['Interest'].unique() 
+interest_map = {} 
+for i in range(len(interest_names)):
+    interest_map[interest_names[i]] = i  
 data['Interest_encoded'] = data['Interest'].map(interest_map)
 
-# 4. Select features (inputs) and target (output)
-X = data[['Age', 'Gender']]  # Features: Age and Gender
-y = data['Interest_encoded']  # Target: Encoded Interest
 
-# 5. Train the model
+X = data[['Age', 'Gender']] 
+y = data['Interest_encoded']  
+
+
 model = DecisionTreeClassifier()
 model.fit(X, y)
 
-# 6. Save the trained model and the mapping
+
 joblib.dump(model, 'model.pkl')
 joblib.dump(interest_map, 'interest_map.pkl')
 
